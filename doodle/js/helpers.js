a0981ba88d52a3e0e39c0453a7503a080c23bb07
kevando <khabich@gmail.com>
2019-11-11 01:43:36 -0800

    wip

--- doodle/js/helpers.js
+++ doodle/js/helpers.js
@@ -1,12 +1,6 @@
 function log(text) {
-
-//   var p = document.createElement("p");
-//   p.innerHTML = text
-//   p.id = 'logger'
-//   console.log(document.body)
-//   document.body.appendChild(p);
+  var p = document.createElement("p");
+  p.innerHTML = text;
+  p.id = "logger";
+  document.body.appendChild(p);
 }
-
-
-
-// log('hio')
